93423dc87e38593c8a8353eda276606d3854b99d
01Leviathan10 <markomilicevic@Markos-MacBook-Pro.local>
2018-01-07 11:51:04 +0100

    Guess

---
+++ js/Basic/guessing game/guessing.js
@@ -0,0 +1,16 @@
+// Create seacretNumber
+
+var seacretNumber = 4;
+
+//ask user for guess
+
+var guess = prompt("Guess a number");
+
+//check guess
+
+if (guess == seacretNumber) {
+    alert("You guesst it!!");
+}
+else{
+    alert("You are wrong!");
+}
---
+++ js/Basic/test/condition.js
@@ -0,0 +1,20 @@
+"use strict"
+
+var age = Number(prompt("What is your age"));
+
+
+if (age === 21) {
+    console.log("Happy 21st birthday!!");
+
+}
+if (age < 0) {
+    console.log("Error");
+
+}
+if ((age % 2) != 0) {
+    console.log("Your age is odd");
+}
+
+if (age % Math.sqrt(age) === 0) {
+    console.log("perfect square");
+}
