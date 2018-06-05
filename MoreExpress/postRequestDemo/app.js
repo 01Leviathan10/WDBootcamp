29057e689b0945788ceff0efa86faf86bf63a0c7
01Leviathan10 <marko.milicevic0092@gmail.com>
2018-06-05 11:59:57 +0200

    Don for now

--- MoreExpress/postRequestDemo/app.js
+++ MoreExpress/postRequestDemo/app.js
@@ -1,21 +1,27 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
+
+var friends = ["Marko", "Darko", "Zarko", "Jova", "Marko"];
 
 
 app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.post("/addfriend", function (req, res) {
-    res.send("YOU HAVE REACHED THE POS ROUTE!!!")
+app.post("/addfriend", function(req, res) {
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    console.log(friends);
+    res.redirect("/friends");
 });
 
-app.get("/friends", function (req, res) {
-    var friends = ["Marko", "Darko", "Zarko", "Jova", "Marko"];
+app.get("/friends", function(req, res) {
     res.render("friends", {friends: friends});
-
 });
 
 
